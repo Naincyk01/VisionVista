5d8b270bda12dc1075c3aa4071c97c41b7486c68
Naincy <naincyk15042004@gmail.com>
2024-06-06 20:39:50 +0530

    Wrote some more methods in user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,7 +3,12 @@ import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { 
     loginUser, 
-    logoutUser, 
+    logoutUser,
+    changeCurrentPassword, 
+    updateAccountDetails,
+    getCurrentUser,
+    updateUserAvatar, 
+    updateUserCoverImage, 
     registerUser,refreshAccessToken
 } from "../controllers/user.controller.js";
 
@@ -21,11 +26,15 @@ router.route("/register").post(
     registerUser
 );
 router.route("/login").post(loginUser)
-router.route("/refresh-token").post(refreshAccessToken)
+
 
 //secured routes
 router.route("/logout").post(verifyJWT,  logoutUser)
-
-
+router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
 
 export default router;
