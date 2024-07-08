5dd90c2476a6c1c3b75916989ea8e4158b8020eb
Naincy <naincyk15042004@gmail.com>
2024-07-08 18:47:56 +0530

    Added addComment method in comment controller

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -3,6 +3,8 @@ import {Comment} from "../models/comment.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
+import { Video } from "../models/video.model.js";
+
 
 const getVideoComments = asyncHandler(async (req, res) => {
     const {videoId} = req.params
@@ -11,16 +13,42 @@ const getVideoComments = asyncHandler(async (req, res) => {
 })
 
 const addComment = asyncHandler(async (req, res) => {
-    
-})
+    const { videoId } = req.params;
+    const user = req.user;
+    const content = req.body?.content;
+
+    if (!content || !videoId || !user) {
+        throw new ApiError(400, "Content, videoId, and user information are required.");
+    }
+
+    const video = await Video.findById(videoId);
+    const newcomment = await Comment.create({
+        content,
+        video,
+        owner: user,
+      });
+
+      const addComment = await Comment.findById(newcomment._id);
+      if (!addComment) {
+        throw new ApiError(500, "Something went wrong while adding the comment");
+      }
+
+    res.json(new ApiResponse(201, "Comment added successfully"));
+});
+
+
+
 
 const updateComment = asyncHandler(async (req, res) => {
    
 })
 
+
+
 const deleteComment = asyncHandler(async (req, res) => {
-    
-})
+   
+});
+
 
 export {
     getVideoComments, 
