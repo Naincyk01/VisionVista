d7100ad9cacfbddcea3d24221899e8ceabc411d2
Naincy <naincyk15042004@gmail.com>
2024-07-10 12:26:57 +0530

    Set up routes for playlist controller

--- src/app.js
+++ src/app.js
@@ -28,6 +28,7 @@ import commentRouter from "./routes/comment.routes.js"
 import tweetRouter from "./routes/tweet.routes.js";
 import likeRouter from "./routes/like.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
+import playlistRouter from "./routes/playlist.routes.js"
 
 //routes declarartion
 app.use("/api/v1/users", userRouter);
@@ -36,7 +37,7 @@ app.use("/api/v1/comments", commentRouter)
 app.use("/api/v1/tweets", tweetRouter);
 app.use("/api/v1/likes", likeRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
-
+app.use("/api/v1/playlist", playlistRouter)
 export { app };
 
 //http://localhost:8000/api/v1/users/register
